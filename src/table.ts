798d821b4b8969e30711783b8bdf0971d4a00c1b
Sai Jonnala <jspreddy@users.noreply.github.com>
2024-04-08 00:34:40 -0400

    prototype query interface with state

--- src/table.ts
+++ src/table.ts
@@ -1,15 +1,81 @@
 export class Table {
-    name: string;
-    hashKey: string;
-    rangeKey: string;
-    something: string;
 
-    constructor(name: string, hashKey: string, rangeKey: string) {
-        this.name = name;
-        this.hashKey = hashKey;
-        this.rangeKey = rangeKey;
-        // this.something = "blah";
+    private _name: string;
+    private _hashKey: string;
+    private _rangeKey: string;
+    private _selections: Array<string>;
+    private _hashVal: string;
+    private _rangeVal: string;
+    private _filters: Array<object>;
+    private _index: string;
+    private _limit: number;
+
+    get state() {
+        return {
+            name: this._name,
+            hashKey: this._hashKey,
+            rangeKey: this._rangeKey,
+            selections: this._selections,
+            hashVal: this._hashVal,
+            rangeVal: this._rangeVal,
+            filters: this._filters,
+            index: this._index,
+            limit: this._limit,
+        };
     }
 
+    constructor(name: string, hashKey: string, rangeKey: string) {
+        this._name = name;
+        this._hashKey = hashKey;
+        this._rangeKey = rangeKey;
+
+        // initialize here so that each Table obj has its own filter list.
+        this._filters = [];
+    }
+
+    select(cols: Array<string>) {
+        this._selections = cols;
+        return this;
+    }
+
+    get where() {
+        const whereSelectors = {
+            hash: {
+                eq: (val: string): Table => {
+                    this._hashVal = val;
+                    return this;
+                }
+            },
+            range: {
+                eq: (val: string): Table => {
+                    this._rangeVal = val;
+                    return this;
+                }
+            },
+        };
+        return whereSelectors;
+    }
+
+    get filter() {
+        const filterConditions = {
+            eq: (attrib: string, val: any): Table => {
+                this._filters.push({ attrib, val, type: 'eq' });
+                return this;
+            }
+        };
+        return filterConditions;
+    }
+
+    using(index: string) {
+        this._index = index;
+    }
+
+    limit(l: number) {
+        this._limit = l | 100;
+    }
+
+    toDynamoQuery() {
+        return this.state;
+    }
 
 }
--- tests/unit-tests/table.test.js
+++ tests/unit-tests/table.test.js
@@ -3,8 +3,35 @@ import { Table } from '../../src/table';
 describe('class: Table', () => {
     it('should initialize with values', async () => {
         const x = new Table('some-table-name', 'pk', 'sk');
-        expect(x.name).toBe('some-table-name');
-        expect(x.hashKey).toBe('pk');
-        expect(x.rangeKey).toBe('sk');
+        expect(x.state).toMatchObject({
+            name: 'some-table-name',
+            hashKey: 'pk',
+            rangeKey: 'sk'
+        });
+    });
+
+    it('should have proper state after chaining methods', async () => {
+        const x = new Table('some-table-name', 'pk', 'sk');
+        x.select(['asdf', 'pqrs'])
+            .where.hash.eq('aasdf')
+            .where.range.eq('1235:238h9084')
+            .filter.eq('flower', 'rose')
+            .filter.eq('isPolinated', true)
+            .limit(10);
+
+        expect(x.state).toEqual({
+            name: 'some-table-name',
+            hashKey: 'pk',
+            rangeKey: 'sk',
+            selections: ['asdf', 'pqrs'],
+            hashVal: 'aasdf',
+            rangeVal: '1235:238h9084',
+            filters: [
+                { attrib: 'flower', val: 'rose', type: 'eq' },
+                { attrib: 'isPolinated', val: true, type: 'eq' }
+            ],
+            index: undefined,
+            limit: 110
+        });
     });
 });
